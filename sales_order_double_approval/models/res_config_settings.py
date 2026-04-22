fb233ed53d8719ecd4f79bbe0b27df19a8dd9268
alihassanmostafa <alihassan.mostafa@gmail.com>
2026-04-22 21:11:32 +0200

    .

--- sales_order_double_approval/models/res_config_settings.py
+++ sales_order_double_approval/models/res_config_settings.py
@@ -1,26 +1,19 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 #############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
-#
-#    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
+# Cybrosys Technologies Pvt. Ltd.
+# Copyright (C) 2024-TODAY Cybrosys Technologies(https://www.cybrosys.com)
+# Author: Cybrosys Techno Solutions(https://www.cybrosys.com)
+# You can modify it under the terms of the GNU LESSER
+# GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+# You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+# (LGPL v3) along with this program.
+# If not, see http://www.gnu.org/licenses/.
 #############################################################################
 from odoo import api, fields, models
-
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
@@ -29,36 +22,27 @@ class ResConfigSettings(models.TransientModel):
         string="Sale Order Approval",
         help="Enable this option to require double validation for sale orders. "
              "When enabled, sale orders exceeding the specified minimum amount "
-             "will require approval by a sales manager."
+             "will require approval by a sales manager or approver."
     )
     so_min_amount = fields.Monetary(
         string="Minimum Amount",
         help="Specify the minimum amount that triggers the double validation "
              "for sale orders. Sale orders exceeding this amount will require "
-             "approval by a sales manager."
+             "approval by a sales manager or approver."
     )
 
     @api.model
     def get_values(self):
-        """
-         Override to get the values of the custom fields from the 'ir.config_parameter' model.
-         """
         res = super(ResConfigSettings, self).get_values()
-        res['so_approval'] = self.env['ir.config_parameter'].sudo().get_param(
-            "sales_order_double_approval.so_approval", default="")
-        res['so_min_amount'] = self.env['ir.config_parameter'].sudo().get_param(
-            "sales_order_double_approval.so_min_amount", default="")
+        icp = self.env['ir.config_parameter'].sudo()
+        # Ensure keys do not have trailing spaces
+        res['so_approval'] = icp.get_param('sales_order_double_approval.so_approval', default=False)
+        res['so_min_amount'] = float(icp.get_param('sales_order_double_approval.so_min_amount', default=0.0))
         return res
 
-    @api.model
     def set_values(self):
-        """
-        Override to set the values of the custom fields in the 'ir.config_parameter' model.
-        """
-        self.env['ir.config_parameter'].set_param(
-            "sales_order_double_approval.so_approval",
-            self.so_approval or '')
-        self.env['ir.config_parameter'].set_param(
-            "sales_order_double_approval.so_min_amount",
-            self.so_min_amount or '')
         super(ResConfigSettings, self).set_values()
+        icp = self.env['ir.config_parameter'].sudo()
+        # Ensure keys do not have trailing spaces
+        icp.set_param('sales_order_double_approval.so_approval', self.so_approval)
+        icp.set_param('sales_order_double_approval.so_min_amount', self.so_min_amount)
--- sales_order_double_approval/models/sale_order.py
+++ sales_order_double_approval/models/sale_order.py
@@ -1,60 +1,74 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 #############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
-#
-#    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
+# Cybrosys Technologies Pvt. Ltd.
+# Copyright (C) 2024-TODAY Cybrosys Technologies(https://www.cybrosys.com)
+# Author: Cybrosys Techno Solutions(https://www.cybrosys.com)
+# You can modify it under the terms of the GNU LESSER
+# GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+# You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+# (LGPL v3) along with this program.
+# If not, see http://www.gnu.org/licenses/.
 #############################################################################
-from odoo import fields, models
-
+from odoo import fields, models, _
+from odoo.exceptions import UserError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    state = fields.Selection(selection_add=
-                             [('to_approve', 'To Approve'),
-                              ('sent',)], ondelete={'to_approve': 'cascade'})
+    state = fields.Selection(selection_add=[
+        ('to_approve', 'To Approve')
+    ], ondelete={'to_approve': 'set default'})
 
     def button_approve(self):
         """
-         Method to approve the sale order and change its state to 'sale'.
-          """
+        Method to approve the sale order and change its state to 'sale'.
+        Only users with Sales Manager or Sale Order Approver rights can call this.
+        """
+        # Check if user has permission to approve
+        if not (self.user_has_groups('sales_team.group_sale_manager') or 
+                self.user_has_groups('your_module_name.group_sale_order_approver')):
+            raise UserError(_("You do not have permission to approve this sale order."))
+        
         self.write({'state': 'sale'})
+        return True
 
     def action_confirm(self):
         """
         Override to add double validation logic based on company settings.
         Confirms the sale order if conditions are met, otherwise sets state to 'to_approve'.
-         """
-        res = super(SaleOrder, self).action_confirm()
-        if self.company_id.so_double_validation:
-            if self.env['ir.config_parameter'].sudo().get_param(
-                    'sales_order_double_approval.so_approval'):
-                if self.amount_total > float(
-                        self.env['ir.config_parameter'].sudo().get_param(
-                            'sales_order_double_approval.so_min_amount')):
-                    if self.user_has_groups('sales_team.group_sale_manager'):
-                        self.state = 'sale'
-                    else:
-                        self.state = 'to_approve'
-        return res
+        """
+        # Get configuration parameters
+        ir_config = self.env['ir.config_parameter'].sudo()
+        so_approval_enabled = ir_config.get_param('sales_order_double_approval.so_approval', default=False)
+        try:
+            min_amount = float(ir_config.get_param('sales_order_double_approval.so_min_amount', default=0.0))
+        except ValueError:
+            min_amount = 0.0
+
+        # Check if double validation is enabled and amount exceeds limit
+        if so_approval_enabled and self.amount_total > min_amount:
+            # Check if current user is Sales Manager OR Sale Order Approver
+            is_manager = self.user_has_groups('sales_team.group_sale_manager')
+            is_approver = self.user_has_groups('your_module_name.group_sale_order_approver')
+            
+            if is_manager or is_approver:
+                # User has rights, confirm directly
+                return super(SaleOrder, self).action_confirm()
+            else:
+                # User does not have rights, set to To Approve
+                self.write({'state': 'to_approve'})
+                return True
+        else:
+            # Normal confirmation flow
+            return super(SaleOrder, self).action_confirm()
 
     def action_cancel(self):
         """
         Method to cancel the sale order and change its state to 'cancel'.
         """
-        self.state = 'cancel'
+        self.write({'state': 'cancel'})
+        return True
